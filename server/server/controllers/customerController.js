0ae7c78ac067a73536a637d74478541da51e7402
Musab Sakhreyah <mosabkhaled1998@gmail.com>
2024-02-26 17:13:11 +0300

    setup customer route and controller

---
+++ server/server/controllers/customerController.js
@@ -0,0 +1,21 @@
+const restaurant = require('../models/restaurantModel');
+
+const getRestaurant = async (_, res) => {
+    try {
+        const lstRestaurant = await restaurant.find({});
+
+        res.status(200).json({
+            message: 'List of Restaurant Results',
+            Restaurant: lstRestaurant,
+        });
+    } catch (err) {
+        res.status(422).json({
+            message: 'Error while getting Restaurant list',
+            error: err.message,
+        });
+    }
+};
+
+module.exports = {
+    getRestaurant,
+};
