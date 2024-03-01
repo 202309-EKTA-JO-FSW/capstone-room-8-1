84d84c0fb7d5d0e28380c3cced22afba15a72c9a
karbejha <mohamad.karbejha@gmail.com>
2024-03-01 21:53:23 +0300

    move the apis from the folder of customers to admins

--- server/server/routes/admin.js
+++ server/server/routes/admin.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
+const customerController = require('../controllers/customerController');
 
 router.get('/getRestaurant', adminController.getRestaurant);
 router.post('/addNewRestaurant', adminController.addNewRestaurant);
+
+router.get('/customers', customerController.getCustomers);
+router.get('/customers/:id', customerController.getCustomerById);
+router.get('/orders/:id', customerController.getOrdersById);
+router.get('/orders/:id', customerController.getOrderById);
 
 // router.put("/admin/item/:id", adminController.updateItem);
 
