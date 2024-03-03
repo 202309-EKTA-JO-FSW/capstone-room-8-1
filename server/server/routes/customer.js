3690beeda604de0405532fc66db6b28e68862a5a
Musab Sakhreyah <mosabkhaled1998@gmail.com>
2024-03-04 01:42:39 +0300

    add review + cart routes

--- server/server/routes/customer.js
+++ server/server/routes/customer.js
@@ -2,6 +2,18 @@ const express = require('express');
 const router = express.Router();
 const customerController = require('../controllers/customerController');
 
-router.get('/getRestaurant', customerController.getRestaurant);
+// router.get('/getCustomers', customerController.getCustomers);
+// router.get('/getCustomerById/:id', customerController.getCustomerById);
+// router.get('/getOrdersById/:id', customerController.getOrdersById);
+// router.get('/getOrderById/:id', customerController.getOrderById);
+
+router.get('/review/:orderId', customerController.getReviewForOrder);
+router.post('/review/:orderId', customerController.postReviewForOrder);
+
+router.get('/cart', customerController.getCart);
+router.post('/cart', customerController.addToCart);
+router.delete('/cart', customerController.deleteCart);
+
+router.post('/cart/checkout', customerController.checkout);
 
 module.exports = router;
