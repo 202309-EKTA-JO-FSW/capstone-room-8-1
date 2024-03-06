0f18e2af64be47d95d8845a07fbc8df284f0ec2d
Musab Sakhreyah <mosabkhaled1998@gmail.com>
2024-03-06 08:41:00 +0300

    add signout route

--- server/server/routes/public.js
+++ server/server/routes/public.js
@@ -2,13 +2,11 @@ const express = require('express');
 const router = express.Router();
 const publicController = require('../controllers/publicController');
 
-// const isAuthorized = require('../middleware/isAuthorized');
-// const isAuthenticated = require('../middleware/isAuthenticated');
-
 router.get('/restaurants', publicController.getRestaurants);
 router.get('/restaurants/:id/dishes', publicController.getDishes);
 router.post('/signUp', publicController.signUp);
 router.post('/signIn', publicController.signIn);
+router.get('/signout', publicController.signOut);
 router.get('/reviews/:restaurantId', publicController.getReviewsByRestaurantId);
 
 module.exports = router;
