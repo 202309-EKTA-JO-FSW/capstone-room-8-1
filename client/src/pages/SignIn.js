176668c8ce5791346698145edea4c99590fb2445
Noor Alrai <ameinnoor91@gmail.com>
2024-03-09 12:45:41 +0300

    add code to pages

---
+++ client/src/pages/SignIn.js
@@ -0,0 +1,7 @@
+
+import SignIn from "../components/SignIn"
+import '../app/globals.css'
+export default function SignInPage() {
+    return ( 
+        <SignIn/>
+    )}
---
+++ client/src/pages/SignUp.js
@@ -0,0 +1,7 @@
+
+import SignUp from "../components/signUp";
+import '../app/globals.css'
+export default function SignUpPage() {
+    return ( 
+        <SignUp/>
+    )}
