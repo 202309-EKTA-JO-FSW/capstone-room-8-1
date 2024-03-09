ef66748ea72faf940c66d8cd6dd876197a0eacd8
Noor Alrai <ameinnoor91@gmail.com>
2024-03-09 12:58:48 +0300

    install bcryptjs

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,14 +1,11 @@
-import Test from './components/Test'
-import SignUp from './components/signUp'
-import SignIn from "./components/SignIn";
 
+import Navbar from "../components/Navbar"
 
 export default function Home () {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <SignUp/>
-        <SignIn/>
+      <Navbar/>
       </div>
     </main>
   )
